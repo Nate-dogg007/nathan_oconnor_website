661b3a97c94c836b858218b6b816745b835854f1
Nate-dogg007 <40372153+Nate-dogg007@users.noreply.github.com>
2025-08-11 23:41:24 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    optimizeCss: true,
+    cssChunking: 'strict',
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 }
 
 export default nextConfig
